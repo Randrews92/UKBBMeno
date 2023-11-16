f784052f7783e364f45c690e12dc9b3388a5018f
Robin <you@example.com>
2023-11-16 12:52:35 +0000

    Saving workspaces in UKBB

---
+++ R workspace saving.R
@@ -0,0 +1,8 @@
+# Saving environments
+
+# In terminal put dx-backup-folder
+
+# It should say Folder . was successfully saved to....
+
+# To restore in Temrinal put (or whatever the name of your file is): 
+#dx-restore-folder /.Backups/rstudio_workbench_ukbrap_trial.Randrews92.2023-11-16T12-41-43.tar.gz
