b602237417a98a7fe5a6a6bd02e358d959bc45a1
Eve <DouglasEA@cardiff.ac.uk>
2024-01-31 09:00:36 +0000

    added code

--- EndogLOE_code.R
+++ EndogLOE_code.R
@@ -18,71 +18,6 @@ library("cli")
 library("pillar")
 
 
-EndogLong <- EndogenousLOE_DeathAge_participant %>%
-  pivot_longer(
-    cols = 3:6,
-    names_to = "Had menopause-instance",
-    values_to = "Had menopause"
-  )
-
-EndogLong1 <- EndogLong %>%
-  pivot_longer(
-    cols = 3:6,
-    names_to = "Age at menopause-instance",
-    values_to = "Age at menopause"
-  )
-EndogLong2 <- EndogLong1 %>%
-  pivot_longer(
-    cols = 3:6,
-    names_to = "Age at menarche-instance",
-    values_to = "Age at menarche"
-  )
-
-EndoLong1 <- EndogLong1 %>%
-  mutate(
-    `Age when periods started (menarche) | Instance 1` = as.character(`Age when periods started (menarche) | Instance 1`),
-    `Age when periods started (menarche) | Instance 3` = as.character(`Age when periods started (menarche) | Instance 3`)
-  ) %>%
-  pivot_longer(cols = c(`Age when periods started (menarche) | Instance 0`, `Age when periods started (menarche) | Instance 1`, `Age when periods started (menarche) | Instance 2`, `Age when periods started (menarche) | Instance 3`), 
-               names_to = "Age at Menarche instance", 
-               values_to = "Age at Menarche")
-
-EndogLong2 <- EndoLong1 %>%
-  mutate(
-    `Number of live births | Instance 1` = as.character(`Number of live births | Instance 1`),
-    `Number of live births | Instance 2` = as.character(`Number of live births | Instance 2`),
-    `Number of live births | Instance 3` = as.character(`Number of live births | Instance 3`)
-  ) %>%
-  pivot_longer(cols = c(`Number of live births | Instance 0`, `Number of live births | Instance 1`, `Number of live births | Instance 2`, `Number of live births | Instance 3`), 
-               names_to = "Number of live births instance", 
-               values_to = "Number of live births")
-
-EndogLong3 <- EndogLong2 %>%
-  pivot_longer(
-    cols = 3:4,
-    names_to = "Age at death-instance",
-    values_to = "Age at death"
-  )
-
-#filter by women:
-EndolongFemale= EndogLong3Long%>%filter(grepl("female", Sex))
-EndolongFem= EndolongFemale %>% filter(!is.na(Sex))
-
-#filter by death:
-DF1= EndolongFem %>% filter(!is.na(ageAtDeath))
-DF2= DF1 %>% filter(ageAtDeath,<0)
-
-#Had menopause
-DF3= DF2%>%filter(grepl("Yes", HadMenopause))
-
-#Remove NAs
-DF4= DF3 %>% filter(!is.na(ageAtMenopause))
-DF5= DF4 %>% filter(!is.na(ageAtMenarche))
-
-#NAs --> 0 in live births
-Df6<- Df5%>%
-  mutate(livebirths1 = ifelse(is.na(livebirths), 0, livebirths))
-
 mutated_data <- EndogenousLOE_DeathAge_participant %>%
   mutate(across(starts_with("Age when periods started (menarche)"), as.character),
          across(starts_with("Number of live births"), as.character),
@@ -115,8 +50,49 @@ pivoted_data <- mutated_data %>%
     values_to = "Age at death"
   )
 
-library(data.table)
 
+#filter by women:
+women_only <- pivoted_data %>%
+  filter(str_detect(Sex, "(?i)female"))
+
+#filter by death:
+deceased_data <- women_only %>%
+  filter(!is.na(`Age at death`))
+
+#Had menopause
+menopausal_data <- deceased_data %>%
+  filter(`Had menopause` == "Yes")
+
+#Remove NAs
+cleaned_data <- menopausal_data %>%
+  filter(!is.na(`Age at menopause`))
+cleaned_data2 <- cleaned_data %>%
+  filter(!is.na(`Age at Menarche`))
+
+#NAs --> 0 in live births
+cleaned_data3 <- cleaned_data2 %>%
+  mutate(`Number of live births` = ifelse(is.na(`Number of live births`), 0, `Number of live births`))
+
+#issue is that if they have answered in instance 1 the other instances are NA and are now 0. 
+
+# Remove Don't knows/prefer not to answer
+cdata4 <- cleaned_data3 %>%
+  filter(`Age at menopause` != "Do not know")
+cdata5 <- cdata4 %>%
+  filter(`Age at Menarche` != "Do not know")
+
+# Convert to numeric form
+numeric_data <- cdata5 %>%
+  mutate(
+    `Age at menopause` = as.numeric(`Age at menopause`),
+    `Age at Menarche` = as.numeric(`Age at Menarche`),
+    `Number of live births` = as.numeric(`Number of live births`),
+    `Age at death` = as.numeric(`Age at death`)
+  )
+
+
+
+#Melt function attempt
 
 setDT(mutated_data)  # Convert to data.table
 
