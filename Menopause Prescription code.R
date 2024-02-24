0186d94c368a695072011ce6c63ecf34cbb51e84
Eve <DouglasEA@cardiff.ac.uk>
2024-02-24 18:47:50 +0000

    some added code

--- Menopause Prescription code.R
+++ Menopause Prescription code.R
@@ -5,11 +5,15 @@ subset_data <- T %>%
   select(`Participant.ID`,
          `menopauseAge`,
          `Year.of.birth`,
-         `Had.menopause`)
+         `Had.menopause`,
+         'Date.of.attending.assessment.centre...Instance.0')
+
 subset_data <- rename(subset_data, "Participant ID" = Participant.ID)
 A = left_join(subset_data, Med_table_participant, by = "Participant ID")
 
 filter_subset <- A%>% filter(grepl("Yes", Had.menopause))
+filter_subset  <- filter_subset  %>%
+  select(-"Date of attending assessment centre | Instance 0")
 
 # Remove rows with missing values
 filter_subset <- filter_subset[complete.cases(filter_subset$Year.of.birth, filter_subset$menopauseAge), ]
@@ -26,15 +30,15 @@ keyword <- "assessment"
 matching_columns <- grep(keyword, names(filter_subset), value = TRUE)
 print(matching_columns)
 
-# Convert the 'Date of attending assessment centre | Instance 0' column to Date type
-filter_subset$'Date of attending assessment centre | Instance 0' <- as.Date(filter_subset$'Date of attending assessment centre | Instance 0')
+# Convert the 'Date.of.attending.assessment.centre...Instance.0' column to Date type
+filter_subset$'Date.of.attending.assessment.centre...Instance.0' <- as.Date(filter_subset$'Date.of.attending.assessment.centre...Instance.0')
 
 # Filter the data
 filtered_df <- filter_subset %>%
   mutate(Menopause_Year = as.integer(MenopauseYear)) %>%
   filter(
-    `Date of attending assessment centre | Instance 0` >= (as.Date(as.character(Menopause_Year), "%Y") - years(5)) &
-      `Date of attending assessment centre | Instance 0` <= (as.Date(as.character(Menopause_Year), "%Y") + years(5))
+    Date.of.attending.assessment.centre...Instance.0 >= (as.Date(as.character(Menopause_Year), "%Y") - years(5)) &
+      Date.of.attending.assessment.centre...Instance.0 <= (as.Date(as.character(Menopause_Year), "%Y") + years(5))
   )
 
 
