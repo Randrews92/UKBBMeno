8be8b334a03b3ed7906db1579ea40ad1c4c14fe9
Your Name <you@example.com>
2024-05-02 13:29:07 +0000

    APOE status and matching on ID.

--- GWAS apoe-4 extraction.R
+++ GWAS apoe-4 extraction.R
@@ -1,71 +1,129 @@
 # Load necessary library
-install.packages('dbplyr')
-library(dbplyr)
-install.packages('bigsnpr')
-library(bigsnpr)
-install.packages('here')
-library('here')
+
 library(yaml)
-install.packages('kableExtra')
-library(kableExtra)
 library(data.table)
 library(dplyr)
 library(stringr)
 library(foreign)
-install.packages('RSQLite')
-library(RSQLite)
-19_44908822_C_T, 19_44908684_T_C
-# Define paths and SNP IDs
-bgenfiles <- "ukb22828_c19_b0_v3.bgen"
-backingfile <- "apoe"
-list_snp_id <- list(c("19_44908684_T_C", "19_44908822_C_T"))  # Corrected SNP IDs
+#install.packages("kableExtra")
+library(kableExtra)
+
+## Swiss Army Knife to extract filtered SNPs:
+# First step is to create the apoe.gen and snp-stats.txt files.
+# This needs to be done using command line interface in the RAP
+# Go to Swiss Army Knife in tools library and select your project as output (input won't be required)
+# When you get to the Command Line input this code: echo "rs429358" > rsidlist.txt echo "rs7412" >> rsidlist.txt
+# This will create rsidlist.txt
+# Go into Project > Bulk >  Imputation > UKB Imputation from Genotype
+# Select Start Analysis in top right corner 
+# Input files must be ukb22828_c19_b0_v3.bgen, rsidlist.txt, ukb22828_c19_b0_v3.bgen.bgi, ukb22828_c19_b0_v3.sample
+# Input this in the Command Line: bgenix -g ukb22828_c19_b0_v3.bgen -incl-rsids rsidlist.txt > apoe.bgen
+# This will create apoe.bgen
+# Go into Project > Bulk >  Imputation > UKB Imputation from Genotype
+# Select Start Analysis in top right corner 
+# Input files must be apoe.bgen
+# Input this code in command line: qctool -g apoe.bgen -snp-stats -osnp snp-stats.txt
+# This will create snp-stats.txt
+# Go into Project > Bulk >  Imputation > UKB Imputation from Genotype
+# Select Start Analysis in top right corner 
+# Input files:snp-stats.txt, apoe.bgen
+# Input this code into Command Line: qctool -g apoe.bgen -og apoe.gen
+# This will create apoe.gen
+
+# NOTE: file names in Bulk are project specific, so for example "ukb22828_c19_b0_v3.bgen" will differ from other projects/ UKBB applications
+
+## The next steps can be completed in R studio
+
+# In terminal run: 
+# dx download apoe.gen
+# dx download snp-stats.txt
+# dx download ukb22828_c19_b0_v3.sample
+
+# Define the file path to your apoe.gen file
+file_path <- "apoe.gen"
+
+# Load the data from the apoe.gen file using fread()
+gen <- fread(file_path)
+
+# Transpose the data so that it's two columns instead of two rows
+gen <- t(gen)
+
+# Define the file path to your .sample file
+sample_file_path <- "ukb22828_c19_b0_v3.sample"
+
+# Read in the sample file using fread()
+sample <- fread(sample_file_path)[-1,]
+
+# Label the columns with the rsID_ref_effect
+colnames(gen) <- paste0(gen[2,], "_", gen[5,], "_", gen[6,])
+
+# Drop the header (first 6 rows: chr, UKB alt_id, rsID, pos, allele1, allele2)
+# In UKB data, allele1 is the reference allele
+gen <- data.frame(gen[-c(1:6),])
+
+# Add participant IDs - there are three rows per participant
+# Each triple of rows is the trio of genotype probabilities for that participant
+gen$ID <- rep(sample$ID_1, each=3)
+gen$prob <- rep(seq(1,3,1),nrow(gen)/3) 
+
+# Split the data into the two SNPs
+# The first genotype probability is homozygous reference allele
+# The second is heterozygous
+# The third is homozygous effect allele
+gen_rs7412 <- gen %>%
+  filter(rs7412_C_T != 0) %>% 
+  mutate(rs7412 = dplyr::case_when(
+    prob==1 ~ "CC",
+    prob==2 ~ "TC",
+    prob==3 ~ "TT",
+    TRUE ~ "Other")) %>%
+  select(ID, rs7412)
+
+gen_rs429358 <- gen %>% 
+  filter(rs429358_T_C != 0) %>%
+  mutate(rs429358 = dplyr::case_when(
+    prob==1 ~ "TT",
+    prob==2 ~ "TC",
+    prob==3 ~ "CC",
+    TRUE ~ "Other")) %>%
+  select(ID, rs429358)
+
+gen_apoe <- dplyr::inner_join(gen_rs429358, gen_rs7412, by="ID")
+
+# Create a summary table to compare with Jennifer Collister's output
 
 
-# Define paths and SNP IDs
-bgenfiles <- "ukb22438_c19_b0_v2.bgen"
-backingfile <- "apoe"
-list_snp_id <- list(c("rs429358", "rs7412"))  # Modify SNP IDs to match the correct format
+genotypes <- data.frame(APOE_genotype = factor(c("e4e4", "e3e4", 
+                                                 "e2e4 / e1e3", "e2e4 / e1e3", 
+                                                 "e3e3", "e2e3", "e2e2", 
+                                                 "e1e4", "e1e2"),
+                                               levels=c("e4e4", "e3e4", 
+                                                        "e2e4 / e1e3",
+                                                        "e3e3", "e2e3", "e2e2", 
+                                                        "e1e4", "e1e2")),
+                        rs429358 = c("CC", "TC", 
+                                     "TC", "CT", 
+                                     "TT", "TT", "TT", 
+                                     "CC", "TC"),
+                        rs7412 = c("CC", "CC", 
+                                   "TC", "TC", 
+                                   "CC", "TC", "TT", 
+                                   "TC", "TT"))
 
+genotypes <- dplyr::inner_join(gen_apoe, genotypes, by=c("rs7412", "rs429358")) %>%
+  mutate(e4 = str_count(APOE_genotype, "4"))
 
-# Read BGEN files into a bigSNP
-snp_readBGEN(
-  bgenfiles = bgenfiles,
-  backingfile = backingfile,
-  list_snp_id = list_snp_id,
-  read_as = "dosage"
-)
+gen_count <- genotypes %>% 
+  count(APOE_genotype, rs429358, rs7412)
 
-# Compute SNP statistics
-bigsnp_obj <- snp_attach(backingfile)
-snp_stats <- snp_stats(bigsnp_obj)
-
-# Save SNP statistics to file
-write.table(snp_stats, "snp-stats.txt", sep = "\t", quote = FALSE, row.names = FALSE)
-
-# Generate genotype data and save to apoe.gen
-make_genotype(bigsnp_obj, "apoe.gen")
-
-
-
-# Write rsids to rsidlist.txt
-cat("rs429358", file = "rsidlist.txt", append = FALSE, sep = "\n")
-cat("rs7412", file = "rsidlist.txt", append = TRUE, sep = "\n")
-
-# Execute bgenix command
-system("/dev/ -incl-rsids rsidlist.txt > apoe.bgen")
-
-# Execute qctool command for SNP stats
-system("/apps/well/qctool/2.0.1/qctool -g apoe.bgen -snp-stats -osnp snp-stats.txt")
-
-# Execute qctool command to generate apoe.gen
-system("/apps/well/qctool/2.0.1/qctool -g apoe.bgen -og apoe.gen")
-
-stats <- read.delim(file.path(config$data$prs, "APOE", "snp-stats.txt"),
-                    sep = " ", comment.char = "#")
-
-kable(stats %>% 
-        select(rsid, alleleA, alleleB, HW_exact_p_value, minor_allele_frequency, impute_info, missing_proportion) %>%
-        rename(HWE_pval = HW_exact_p_value,
-               MAF = minor_allele_frequency),
-      caption="Summary stats") %>%
+kable(gen_count) %>%
   kable_styling(latex_options = "striped")
+
+# Table matches perfectly with Jennifer Collister's output, confirming the data is sound. 
+
+# write csv 
+write.csv(gen_apoe, file = "gen_apoe.csv", row.names = FALSE)
+
+# in Terminal type: dx upload gen_apoe.csv 
+# Now the APOE participant list is saved in Projects and can be joined onto prepped cohorts. 
+
